e146c4ce98fc97bb9236f62bb6320c338a6ec388
skaifun <dev@skai.fun>
2023-04-23 21:54:32 +0800

    feat: 使用 Java 代码 + 注解(Spring+SpringMVC)的方式实现示例
    将 web.xml 删除，然后 AppConfig.java 实现 WebApplicationInitializer 接口，在该接口的 onStartup 方法中配置并注册 DispatcherServlet。

--- src/main/java/fun/skai/example/servlet/AppConfig.java
+++ src/main/java/fun/skai/example/servlet/AppConfig.java
@@ -1,11 +1,25 @@
 package fun.skai.example.servlet;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "fun.skai.example.servlet")
-public class AppConfig {
+public class AppConfig implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        // 基于 Java + 注解的方式
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(AppConfig.class);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+        dispatcher.addMapping("/");
+    }
 }
