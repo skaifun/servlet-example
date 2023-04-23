c1561e1d10d14a4fabf3f2d2d22c0003cff8e784
skaifun <dev@skai.fun>
2023-04-23 21:49:40 +0800

    feat: 使用 web.xml + Java 代码配置 + 注解(Spring+SpringMVC)的方式实现示例
    将 app-context.xml 容器配置改为使用 Java 代码+注解方式的 AppConfig.java，然后删除 app-context.xml 文件。最后在 web.xml 中将 SpringMVC 的DispatcherServlet 初始化参数中 IoC 容器类改为注解配置的 AnnotationConfigWebApplicationContext，并将配置文件位置改为AppConfig的全限定名。

---
+++ src/main/java/fun/skai/example/servlet/AppConfig.java
@@ -0,0 +1,11 @@
+package fun.skai.example.servlet;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "fun.skai.example.servlet")
+public class AppConfig {
+}
